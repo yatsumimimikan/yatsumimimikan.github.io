ab2b5b06e6cdb1c5eab67231975280189ba089b0
yatsumimimikan <niceepitaph@gmail.com>
2018-07-10 00:57:30 +0900

    add

--- Test/index.js
+++ Test/index.js
@@ -1,13 +1,11 @@
 const medias = {audio : false, video : {
-        facingMode : {exact : "environment"}// リアカメラにアクセス
-            }
-      },
-      video  = document.getElementById("video"),
-      canvas = document.getElementById("canvas"),
-      ctx    = canvas.getContext("2d");
+        facingMode : {
+          exact : "environment" // リアカメラにアクセス
+        }
+      }},
+      video  = document.getElementById("video");
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
-requestAnimationFrame(draw);
 
 function successCallback(stream) {
   video.srcObject = stream;
@@ -16,11 +14,3 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
-
-function draw() {
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
-  ctx.drawImage(video, 0, 0);
-
-  requestAnimationFrame(draw);
-}
