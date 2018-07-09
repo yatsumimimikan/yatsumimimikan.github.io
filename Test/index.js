86b425b3dc4049e50ab236b272c99b0081d4f9a3
yatsumimimikan <niceepitaph@gmail.com>
2018-07-10 01:00:09 +0900

    add

--- Test/index.js
+++ Test/index.js
@@ -4,8 +4,13 @@ const medias = {audio : false, video : {
         }
       }},
       video  = document.getElementById("video");
+      canvas = document.getElementById("canvas"),
+      ctx    = canvas.getContext("2d");
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
+
+requestAnimationFrame(draw);
+
 
 function successCallback(stream) {
   video.srcObject = stream;
@@ -14,3 +19,11 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  requestAnimationFrame(draw);
+}
