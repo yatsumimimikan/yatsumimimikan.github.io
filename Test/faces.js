5d8b26f4a470de9cee25f4a24076661a9c805a06
yatsumimimikan <niceepitaph@gmail.com>
2018-07-13 00:15:43 +0900

    add

--- Test/faces.js
+++ Test/faces.js
@@ -30,7 +30,7 @@ function errorCallback(err) {
 
 function FaceTimer(stream) {
 
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     options  = {mediaStream : stream},
     src      = audioCtx.createMediaStreamSource(stream),
     analyser = audioCtx.createAnalyser(stream);
