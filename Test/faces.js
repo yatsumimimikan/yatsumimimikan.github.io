667c0d690698d7284592d612e08639d459ea5fc9
yatsumimimikan <niceepitaph@gmail.com>
2018-07-14 20:59:18 +0900

    add

--- Test/faces.js
+++ Test/faces.js
@@ -13,6 +13,7 @@ mouth_o = new Array("mouth_1.png","mouth_2.png","mouth_3.png");
 mouth_c = new Array("mouth_c_1.png","mouth_c_2.png","mouth_c_3.png");
 faceandbody = new Array("face_1.png","face_2.png","face_3.png");
 count = Math.floor( Math.random() * 1001 ); //*2
+countInt = Math.floor(count);
 
 //口パク
 var hitarea = document.getElementById('hitarea'),
@@ -34,13 +35,14 @@ function FaceTimer() {
     setInterval(() => {
 
 	count -= Math.abs(aX)+Math.abs(aY)+Math.abs(aZ); 
+        countInt = Math.floor(count);
 	if (count <= 0)
             {count = Math.floor( Math.random() * 1001 );
-             document.eyes.src = eye_c[count%3];
+             document.eyes.src = eye_c[countInt%3];
             }
         else
             {
-             document.eyes.src = eye_o[count%3];
+             document.eyes.src = eye_o[countInt%3];
             }
         document.eyes.hspace = 20 + aY*10;
         document.eyes.vspace = 20 - aX*10;
@@ -48,15 +50,15 @@ function FaceTimer() {
        //口パク機能
         if(isTouch == 0)
         {
-             document.mouth.src = mouth_c[count%3];
+             document.mouth.src = mouth_c[countInt%3];
         }
         else
         {
-             document.mouth.src = mouth_o[count%3];
+             document.mouth.src = mouth_o[countInt%3];
         }
 
         document.mouth.hspace = 20 + aY*10;
 
-        document.fath.src = faceandbody[count%3];
+        document.fath.src = faceandbody[countInt%3];
     }, 30);
 }
