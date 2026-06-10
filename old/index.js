de32ed2e46b25edcfc82775608fd6e23a33393c4
yatsumimimikan <yatsumimimikan@gmail.com>
2026-06-10 21:03:31 +0900

    move

---
+++ old/index.js
@@ -0,0 +1,16 @@
+const medias = {audio : false, video : {
+        facingMode : {
+          exact : "environment" // リアカメラにアクセス
+        }
+      }},
+      video  = document.getElementById("video");
+
+navigator.getUserMedia(medias, successCallback, errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
